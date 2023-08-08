322bdb99cf0328baab3233faf0b6a74df1f9a16e
Japabh <125514857+Japabh@users.noreply.github.com>
2023-08-07 22:23:02 -0300

    Add files via upload

---
+++ projeto.py
@@ -0,0 +1,12 @@
+import os
+import pesquisar as p 
+
+
+def realizar_emprestimos():
+    emprestimos = []
+
+    print("------------EMPRESTIMOS DE LIVROS----------")
+    os.system('cls') or None
+
+    nome_usuario = input("Nome do usuário: ")
+    retorno = 
